fc39eb8d0b1fd6ec607e6b78c2e6171b862f4ec7
UsmanAhri <47859269+UsmanAhri@users.noreply.github.com>
2019-08-23 18:01:09 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-const   canvas     = document.getElementById('canvas'),
-        ctx        = canvas.getContext("2d");
+const   canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext("2d");
 
-let score          = 0,
-    bestScore      = 0;
+let score = 0,
+    bestScore = 0;
+
+let startButton = document.getElementById('settings__start');
 
 let settings = {
     box: 10,
     speed: 100,
     width: 600,
-    height: 540,
-    startButton: document.getElementById('settings__start')
+    height: 540
 };
 
 let gameOverBlock = document.querySelector('.game-over-wrap'),
@@ -19,31 +20,34 @@ let gameOverBlock = document.querySelector('.game-over-wrap'),
 let intervalId;
 
 let snake = {
-    direction: 'ArrowRight',
-    size: [],
+    direction: "right",
+    body: [],
     newHead: {},
     move() {
-        let snakeX = snake.size[0].x,
-            snakeY = snake.size[0].y;
+        let snakeX = snake.body[0].x,
+            snakeY = snake.body[0].y;
 
-        if (snake.direction === "ArrowLeft" && snake.direction !== "ArrowRight") {
-            snakeX -= settings.box;
+        switch (snake.direction) {
+            case "left":
+                snakeX -= settings.box;
+                break;
+            case "right":
+                snakeX += settings.box;
+                break;
+            case "up":
+                snakeY -= settings.box;
+                break;
+            case "down":
+                snakeY += settings.box;
+                break;
         }
-        if (snake.direction === "ArrowRight" && snake.direction !== "ArrowLeft") {
-            snakeX += settings.box;
-        }
-        if (snake.direction === "ArrowUp" && snake.direction !== "ArrowDown") {
-            snakeY -= settings.box;
-        }
-        if (snake.direction === "ArrowDown" && snake.direction !== "ArrowUp") {
-            snakeY += settings.box;
-        }
+
         return this.newHead = {
             x: snakeX,
             y: snakeY
         };
     },
-    startSize () {
+    startBody () {
         for (let i = 0; i < 3; i++) {
             let halfWidth = canvas.width / 2;
             let halfHeight = canvas.height / 2;
@@ -52,31 +56,31 @@ let snake = {
                 y: halfHeight - (halfHeight % settings.box),
             };
 
-            snake.size.push(newElem);
+            snake.body.push(newElem);
         }
     },
     snakeTransition(newHead) {
         if (newHead.x === -settings.box) {
             newHead.x = canvas.width;
-            snake.direction = 'ArrowLeft';
+            snake.direction = "left";
         } else if (newHead.x === canvas.width) {
             newHead.x = 0;
-            snake.direction = 'ArrowRight';
+            snake.direction = "right";
         }
 
         if (newHead.y === -settings.box) {
             newHead.y = canvas.height;
-            snake.direction = 'ArrowUp';
+            snake.direction = "up";
         } else if (newHead.y === canvas.height) {
             newHead.y = 0;
-            snake.direction = 'ArrowDown';
+            snake.direction = "down";
         }
     }
 };
 let food = {
-    amount: [],
+    items: [],
     generateFood (count) {
-        while (this.amount.length < count) {
+        while (this.items.length < count) {
             let x = Math.random() * canvas.width,
                 y = Math.random() * canvas.height;
             let newItem = {
@@ -84,38 +88,38 @@ let food = {
                 y: y - (y % settings.box)
             };
             let itemExists = false;
-            for (let i = 0; i < this.amount.length; i++) {
-                if (this.amount[i] === newItem) {
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.items[i] === newItem) {
                     itemExists = true;
                 }
             }
             if (!itemExists) {
-                this.amount.push(newItem);
+                this.items.push(newItem);
             }
         }
-        return this.amount;
+        return this.items;
     },
     generateNewFood () {
         let x = Math.random() * canvas.width,
             y = Math.random() * canvas.height;
-        return newItem = {
+        return {
             x: x - (x % settings.box),
             y: y - (y % settings.box),
         };
     }
 };
 
-settings.startButton.addEventListener("click", function () {
+startButton.addEventListener("click", function () {
     startGame();
 });
 
 function validationCheck(obj) {
-    let minBoxSize      = 1,
-        maxBoxSize      = 3,
-        minSpeed        = 5,
-        maxSpeed        = 1;
+    let minBoxSize = 1,
+        maxBoxSize = 3,
+        minSpeed = 1,
+        maxSpeed = 10;
 
-    // Box size validation
+    // Box body validation
 
     if (obj.box >= minBoxSize && obj.box <= maxBoxSize) {
         obj.box = Math.round(obj.box) * 10;
@@ -128,36 +132,40 @@ function validationCheck(obj) {
 
     // Speed validation
 
-    if (obj.speed <= minSpeed && obj.speed >= maxSpeed) {
-        obj.speed = Math.round(obj.speed) * 100;
+    if (obj.speed >= minSpeed && obj.speed <= maxSpeed) {
+        if (obj.speed === maxSpeed) {
+            obj.speed = 100 * ((maxSpeed + 1) - Math.round(obj.speed));
+        }
+        else {
+            obj.speed = 100 * (maxSpeed - Math.round(obj.speed));
+        }
     }
-    else if (obj.speed > minSpeed) {
-        obj.speed = minSpeed * 100;
+    else if (obj.speed < minSpeed) {
+        obj.speed = minSpeed * 1000;
+        document.getElementById('speed').value = minSpeed;
     }
-    else {
-        obj.speed = maxSpeed * 100;
+    else if (obj.speed > maxSpeed) {
+        obj.speed = maxSpeed * 10;
+        document.getElementById('speed').value = maxSpeed;
     }
 }
 
 function startGame() {
     let personalSettings = {
-        box: document.getElementById('box').value,
-        speed: document.getElementById('speed').value,
+        box: Number(document.getElementById('box').value),
+        speed: Number(document.getElementById('speed').value),
     };
 
     validationCheck(personalSettings);
     settings.box = personalSettings.box;
     settings.speed = personalSettings.speed;
 
-    console.log(personalSettings.speed);
-    console.log(settings.speed);
-
     gameOverBlock.style.display = 'none';
     food.generateFood(3);
-    snake.startSize();
+    snake.startBody();
 
-    document.addEventListener("keydown", directionCode);
-    snake.direction = "ArrowRight";
+    document.addEventListener("keydown", onKeydown);
+    snake.direction = "right";
 
     intervalId = setInterval(snakeMovement, settings.speed);
     setInterval(drawGame, 50);
@@ -172,9 +180,9 @@ function gameOver() {
     gameOverBlock.style.display = 'flex';
     snake.direction = '';
     clearInterval(intervalId);
-    document.removeEventListener("keydown", directionCode);
+    document.removeEventListener("keydown", onKeydown);
 
-    snake.size = [];
+    snake.body = [];
     snake.direction = '';
 
     settings.speed = 100;
@@ -185,26 +193,27 @@ function gameOver() {
     score = 0;
     document.getElementById('settings-best-score').value = bestScore;
 
-    document.getElementById('box').removeAttribute('disabled', 'disabled');
-    document.getElementById('speed').removeAttribute('disabled', 'disabled');
-    document.getElementById('settings__start').removeAttribute('disabled', 'disabled');
+    document.getElementById('box').removeAttribute('disabled');
+    document.getElementById('speed').removeAttribute('disabled');
+    document.getElementById('settings__start').removeAttribute('disabled');
 }
 
 let directionArray = [];
-function directionCode(e) {
-    if (e.code !== directionArray[directionArray.length - 1]) {
 
-        directionArray.push(e.code);
+function onKeydown(event) {
+    let newDirection = event.code.substr(5).toLowerCase();
 
-        if (snake.direction === 'ArrowLeft' && directionArray[0] === 'ArrowRight' ||
-            snake.direction === 'ArrowRight' && directionArray[0] === 'ArrowLeft') {
-            directionArray.unshift('ArrowUp');
-        }
-        else if (snake.direction === 'ArrowUp' && directionArray[0] === 'ArrowDown' ||
-                 snake.direction === 'ArrowDown' && directionArray[0] === 'ArrowUp') {
-            directionArray.unshift('ArrowRight');
-        }
+    if (
+        (snake.direction === newDirection)
+        || (snake.direction === 'left' && newDirection === 'right')
+        || (snake.direction === 'right' && newDirection === 'left')
+        || (snake.direction === 'up' && newDirection === 'down')
+        || (snake.direction === 'down' && newDirection === 'up')
+    ) {
+        return false;
     }
+
+    return directionArray.push(newDirection);
 }
 
 function direction() {
@@ -215,27 +224,29 @@ function direction() {
 }
 
 let eaten = false;
+let eatenFoodIndex;
 
 let snakeMovement = function () {
     direction();
-    for (i = 0; i < food.amount.length; i++) {
-        if (snake.size[0].x === food.amount[i].x && snake.size[0].y === food.amount[i].y) {
+    for (let i = 0; i < food.items.length; i++) {
+        if (snake.body[0].x === food.items[i].x && snake.body[0].y === food.items[i].y) {
             eaten = true;
+            eatenFoodIndex = i;
             break;
         }
     }
     if (eaten) {
-        snake.size.unshift(snake.move());
-        food.amount[i] = food.generateNewFood ();
+        snake.body.unshift(snake.move());
+        food.items[eatenFoodIndex] = food.generateNewFood ();
         score++;
         document.getElementById('settings-score').value = score;
         eaten = false;
     } else {
-        snake.size.unshift(snake.move());
-        snake.size.pop();
+        snake.body.unshift(snake.move());
+        snake.body.pop();
     }
-    for (i = 3; i < snake.size.length; i++) {
-        if (snake.size[0].x === snake.size[i].x && snake.size[0].y === snake.size[i].y) {
+    for (let i = 3; i < snake.body.length; i++) {
+        if (snake.body[0].x === snake.body[i].x && snake.body[0].y === snake.body[i].y) {
             gameOver();
         }
     }
@@ -253,13 +264,13 @@ function drawGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (i = 0; i < food.amount.length; i++) {
+    for (let i = 0; i < food.items.length; i++) {
         ctx.fillStyle = "#ff0000";
-        ctx.fillRect(food.amount[i].x, food.amount[i].y, settings.box, settings.box);
+        ctx.fillRect(food.items[i].x, food.items[i].y, settings.box, settings.box);
     }
 
-    for(i = 0; i < snake.size.length; i++) {
+    for(let i = 0; i < snake.body.length; i++) {
         ctx.fillStyle = "#009000";
-        ctx.fillRect(snake.size[i].x, snake.size[i].y, settings.box, settings.box);
+        ctx.fillRect(snake.body[i].x, snake.body[i].y, settings.box, settings.box);
     }
 }
